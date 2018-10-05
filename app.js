8cef75895500189d9abf3140b0767c3b9e6607cc
Ivan Sardelić <ivan.ssardelic@gmail.com>
2018-10-05 17:54:19 +0200

    Image displays for home page added
    Dynamically rendering images by iterating through the project data (Actual project images have not been added yet)

--- app.js
+++ app.js
@@ -7,14 +7,20 @@ const app = express();
 
 //Adding body parser in order to be able to pass data in requests
 app.use(bodyParser.urlencoded({extended: false}));
+app.use('/static', express.static('public'));
 
 //Selecting the projects from the data file
 const data = require('./data.json');
 const projects = data.projects;
 
-
 //Setting pug as the view engine
 app.set('view-engine', 'pug');
+
+//Creating index route
+
+app.get('/', (req, res, next) => {
+    res.render('index.pug', {projects: projects});
+})
 
 //Starting the app on port 3000
 app.listen(3000, () => {
