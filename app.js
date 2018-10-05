f469ea05a039c57d2480709ca6ee0a85cc3d02f3
Ivan Sardelić <ivan.ssardelic@gmail.com>
2018-10-05 16:58:45 +0200

    Required modules, required data, set view engine, and started app
    Express and BodyParser required, Pug set as the view engine for the project, data has been extracted into a variable from the data file and the app has been started on port 3000

---
+++ app.js
@@ -0,0 +1,22 @@
+//Requiring needed modules (express, body-parser)
+const express = require('express');
+const bodyParser = require('body-parser');
+
+//Creating the app
+const app = express();
+
+//Adding body parser in order to be able to pass data in requests
+app.use(bodyParser.urlencoded({extended: false}));
+
+//Selecting the projects from the data file
+const data = require('./data.json');
+const projects = data.projects;
+
+
+//Setting pug as the view engine
+app.set('view-engine', 'pug');
+
+//Starting the app on port 3000
+app.listen(3000, () => {
+    console.log('App listening on port 3000');
+});
