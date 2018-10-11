d9c9b847c8bb6003a965e05b3f5f61087f35255a
Ivan Sardelić <ivan.ssardelic@gmail.com>
2018-10-11 17:49:56 +0200

    Error handling for project routes added, and comments removed from pug files

--- routes/projects.js
+++ routes/projects.js
@@ -7,10 +7,18 @@ const projects = data.projects;
 
 router.get('/projects/:id', (req, res) => {
     //Submitting an id as a request parameter, which will allow acces to specific elements in the projects array
-    const id = req.params.id;
+    let id = req.params.id;
+
+    //Checking if the given parameter is a valid number, concerning our array of projects
+    //If the parameter isn't valid, then we redirect to a random project
+    if(isNaN(parseInt(id)) || id >= projects.length){
+        id = Math.floor(Math.random() * projects.length);
+        res.redirect(`/projects/${id}`);
+    }
     const project = projects[id];
 
     res.render('project.pug', {project: project});
 })
+
 
 module.exports = router;
